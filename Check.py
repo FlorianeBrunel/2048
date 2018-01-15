b2c6144c3fc137aaf3096a48301bfcb70249f67d
LIUSHU2048 <shuliu586@gmail.com>
2018-01-15 23:51:59 +0100

    Check.py

--- Check.py
+++ Check.py
@@ -8,4 +8,9 @@ def check(self):
 
         print("Vous avez gagné!")
 
-      elif self.plateau
+      elif self.plateaudef check(self):
+    for i in range(0,4):
+        for j in range(0,4):
+            if self.plateau[i,j] == 2048:
+                print("Vous avez gagné!")
+            elif self.plateau
