ca3c1102aed995f29be9658607070813d0702d90
LIUSHU2048 <shuliu586@gmail.com>
2018-01-17 21:22:16 +0100

    Check.py

--- Check.py
+++ Check.py
@@ -5,9 +5,8 @@ Created on Mon Jan 15 23:54:50 2018
 @author: SEa
 """
 
-def check(self):
-    for i in range(0,4):
-        for j in range(0,4):
-            if self.plateau[i,j] == 2048:
-                print("Vous avez gagné!")
-            elif self.plateau
+def check(self,matrice):
+    
+    if 2048 in matrice:
+        print("Vous avez gagné")
+    elif
